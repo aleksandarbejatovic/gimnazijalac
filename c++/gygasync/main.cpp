a03b225fe526e4b13306c81cefb425ef6e8ac6ae
Igor Pehjakovich <gygasync@gmail.com>
2014-03-21 20:05:09 +0100

    v0.3.0
    dodao graficki prikaz kvadra koristeci windows.h biblioteku i linkovao
    sam libgdi.a biblioteku; u CodeBlocksu idite na Settings>Compiler>Lnker
    Settings>Add
    Nadjite gdje vam je Codeblocks ili MINGW instaliran i udjite u
    mingw>lib>libgdi.a

--- c++/gygasync/main.cpp
+++ c++/gygasync/main.cpp
@@ -1,6 +1,11 @@
+//potrebno da bi GetConsoleWindow() radio
+#define _WIN32_WINNT 0x0500
+
 #include <iostream>
 #include <cmath>
 #include <cstdlib>
+#include <windows.h>
+#include <conio.h> //samo zbog sleep funkcije :)
 
 using namespace std;
 
@@ -22,6 +27,39 @@ private:
         Klasa kvadar;
 public:
         void Postavi(double a, double b, double c){x=a;y=b;z=c;};
+        int Crtanje(double ax, double bx, double cx)
+        {
+            system("cls");
+            int visina=(int)(bx*10*sqrt(2)/2);
+            HWND konzola = GetConsoleWindow();
+            HDC dc = GetDC(konzola);
+            COLORREF boja = RGB(255,255,255);
+            COLORREF boja2 = RGB(0,0,255);
+            for(int i=0;i<(int)(ax*10);i++)
+            {
+                SetPixel(dc,i,0+visina,boja);
+                SetPixel(dc,i,(int)(cx*10)+visina,boja);
+                SetPixel(dc,i+visina,0,boja);
+                SetPixel(dc,i+visina,(int)(cx*10),boja2);
+            }
+            for(int i=0;i<(int)(cx*10);i++)
+            {
+                SetPixel(dc,0,i+visina,boja);
+                SetPixel(dc,(int)(ax*10),i+visina,boja);
+                SetPixel(dc,(int)(ax*10)+visina,i,boja);
+                SetPixel(dc,visina,i,boja2);
+            }
+            for(int i=0;i<visina;i++)
+            {
+                SetPixel(dc,i,visina-i+(int)(cx*10),boja2);
+                SetPixel(dc,i,visina-i,boja);
+                SetPixel(dc,i+(int)(ax*10),visina-i,boja);
+                SetPixel(dc,i+(int)(ax*10),visina-i+(int)(cx*10),boja);
+            }
+            ReleaseDC(konzola, dc);
+            Sleep(4000);
+            system("pause");
+        };
         void Prikazi()
         {
             while(p!='n')
@@ -30,7 +68,7 @@ public:
                 cout<<"a="<<x<<"   b="<<y<<"   c="<<z<<endl;
                 if(x==y&&y==z&&x==z)
                     cout<<"Kvadar je Kocka\n"<<endl;
-                cout<<"Izaberi operaciju:\n 1. Zapremina Kvadra\n 2. Povrsina Kvadra\n 3. Glavna dijagonala Kvadra\n 4. Poluprecnik sfere opisane oko kvadra"<<endl;
+                cout<<"Izaberi operaciju:\n 1. Zapremina Kvadra\n 2. Povrsina Kvadra\n 3. Glavna dijagonala Kvadra\n 4. Poluprecnik sfere opisane oko kvadra\n 5. Nacrtaj Kvadar"<<endl;
                 cin>>i;
                 kvadar.Postavi(x,y,z);
                 switch(i)
@@ -55,6 +93,11 @@ public:
                     cout<<"Nastaviti dalje (d/n)? ";
                     cin>>p;
                     break;
+                case 5:
+                    Crtanje(x,y,z);
+                    cout<<"Nastaviti dalje (d/n)? ";
+                    cin>>p;
+                    break;
                 default:
                     break;
                 }
@@ -62,6 +105,7 @@ public:
             system("exit");
         }
 };
+
 
 int main()
 {
