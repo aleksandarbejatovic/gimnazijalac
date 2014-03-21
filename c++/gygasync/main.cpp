beeaf176b16dbef2857375c0d376356a2d801283
Igor Pehjakovich <gygasync@gmail.com>
2014-03-21 16:59:44 +0100

    v0.1.0
    Velika promjena, koriscenje menija koji se nalazi u drugoj klasi, razni
    primjeri objektnog programiranja

--- c++/gygasync/main.cpp
+++ c++/gygasync/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <cstdlib>
 
 using namespace std;
 
@@ -8,30 +9,75 @@ private:
         double x,y,z;
 public:
         void Postavi(double a, double b, double c){x=a;y=b;z=c;};
-        void ZapreminaKvadra(){cout<<"Zapremina Kvadra je: "<<x*y*z<<endl;};
-        void PovrsinaKvadra(){cout<<"Povrsina Kvadra je: "<<2*x*y+2*x*z+2*y*z<<endl;};
-        void GlavnaDijagonalaKvadra(){cout<<"Glavna dijagonala Kvadra je: "<<sqrt(x*x+y*y+z*z)<<endl;};
-        void BoolKocka()
+        void Zapremina(){cout<<"Zapremina Kvadra je: "<<x*y*z<<endl;};
+        void Povrsina(){cout<<"Povrsina Kvadra je: "<<2*x*y+2*x*z+2*y*z<<endl;};
+        void GlavnaDijagonala(){cout<<"Glavna dijagonala Kvadra je: "<<sqrt(x*x+y*y+z*z)<<endl;};
+        void Sfera() {cout<<"Sfera opisana oko kvadra ima poluprecnik: "<<sqrt(x*x+y*y+z*z)/2<<endl;};
+};
+class Meni{
+private:
+        double x,y,z;
+        int i;
+        char p='d';
+        Klasa kvadar;
+public:
+        void Postavi(double a, double b, double c){x=a;y=b;z=c;};
+        void Prikazi()
         {
-            if(x==y&&y==z&&x==z)
-                cout<<"Kvadar je Kocka"<<endl;
+            while(p!='n')
+            {
+                system("cls");
+                cout<<"a="<<x<<"   b="<<y<<"   c="<<z<<endl;
+                if(x==y&&y==z&&x==z)
+                    cout<<"Kvadar je Kocka\n"<<endl;
+                cout<<"Izaberi operaciju:\n 1. Zapremina Kvadra\n 2. Povrsina Kvadra\n 3. Glavna dijagonala Kvadra\n 4. Poluprecnik sfere opisane oko kvadra"<<endl;
+                cin>>i;
+                kvadar.Postavi(x,y,z);
+                switch(i)
+                {
+                case 1:
+                    kvadar.Zapremina();
+                    cout<<"Nastaviti dalje (d/n)? ";
+                    cin>>p;
+                    break;
+                case 2:
+                    kvadar.Povrsina();
+                    cout<<"Nastaviti dalje (d/n)? ";
+                    cin>>p;
+                    break;
+                case 3:
+                    kvadar.GlavnaDijagonala();
+                    cout<<"Nastaviti dalje (d/n)? ";
+                    cin>>p;
+                    break;
+                case 4:
+                    kvadar.Sfera();
+                    cout<<"Nastaviti dalje (d/n)? ";
+                    cin>>p;
+                    break;
+                default:
+                    break;
+                }
+            }
+            system("exit");
         }
-        void SferaKvadra() {cout<<"Sfera opisana oko kvadra ima poluprecnik: "<<sqrt(x*x+y*y+z*z)/2<<endl;};
 };
 
 int main()
 {
     Klasa rad;
+    Meni meni;
     cout<<"Unesi dimenzije"<<endl;
     double k,l,n;
+    int i;
+    cout<<"a=";
     cin>>k;
+    cout<<"b=";
     cin>>l;
+    cout<<"c=";
     cin>>n;
-    rad.Postavi(k,l,n);
-    rad.ZapreminaKvadra();
-    rad.PovrsinaKvadra();
-    rad.GlavnaDijagonalaKvadra();
-    rad.SferaKvadra();
-    rad.BoolKocka();
+    cout<<endl;
+    meni.Postavi(k,l,n);
+    meni.Prikazi();
     return 0;
 }
